18fffc75ce16969d7a375ed78f29a11f34594d51
Sujith <sujith.m1524@gmail.com>
2025-10-23 12:15:13 +0530

    Refactor logo section in Header component for improved accessibility and code consistency

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -11,17 +11,15 @@ export default function Header() {
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
         {/* Logo */}
         <Link to="/" className="text-2xl font-bold text-white tracking-wide hover:scale-105 transition transform font-italic flex gap-1.5">
-        <a href="/">
             <svg width="31" height="34" viewBox="0 0 31 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="m8.75 5.3 6.75 3.884 6.75-3.885M8.75 28.58v-7.755L2 16.939m27 0-6.75 3.885v7.754M2.405 9.408 15.5 16.954l13.095-7.546M15.5 32V16.939M29 22.915V10.962a2.98 2.98 0 0 0-1.5-2.585L17 2.4a3.01 3.01 0 0 0-3 0L3.5 8.377A3 3 0 0 0 2 10.962v11.953A2.98 2.98 0 0 0 3.5 25.5L14 31.477a3.01 3.01 0 0 0 3 0L27.5 25.5a3 3 0 0 0 1.5-2.585" stroke="url(#a)" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="m8.75 5.3 6.75 3.884 6.75-3.885M8.75 28.58v-7.755L2 16.939m27 0-6.75 3.885v7.754M2.405 9.408 15.5 16.954l13.095-7.546M15.5 32V16.939M29 22.915V10.962a2.98 2.98 0 0 0-1.5-2.585L17 2.4a3.01 3.01 0 0 0-3 0L3.5 8.377A3 3 0 0 0 2 10.962v11.953A2.98 2.98 0 0 0 3.5 25.5L14 31.477a3.01 3.01 0 0 0 3 0L27.5 25.5a3 3 0 0 0 1.5-2.585" stroke="url(#a)" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
                 <defs>
                     <linearGradient id="a" x1="15.5" y1="2" x2="15.5" y2="32" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#F8FAFC"/>
-                    <stop offset="1" stop-color="#383838"/>
+                    <stop stopColor="#F8FAFC"/>
+                    <stop offset="1" stopColor="#383838"/>
                     </linearGradient>
                 </defs>
             </svg>
-        </a>
           ProLern
         </Link>
 
