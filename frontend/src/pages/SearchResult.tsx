0f0860b3e165f047efdbf03d360dc231c7298240
Sujith <sujith.m1524@gmail.com>
2025-10-28 15:42:15 +0530

    renamed "Recommendations" section to "Related Courses" in SearchResult component

--- frontend/src/pages/SearchResult.tsx
+++ frontend/src/pages/SearchResult.tsx
@@ -64,7 +64,7 @@ const SearchResult: React.FC<SearchResultProps> = ({ results, userCoords }) => {
                         <ResultsSection title="Exact Matches" courses={exactMatches} userCoords={userCoords} />
                     )}
                     {recommendations.length > 0 && (
-                        <ResultsSection title="Recommendations" courses={recommendations} userCoords={userCoords} />
+                        <ResultsSection title="Related Courses" courses={recommendations} userCoords={userCoords} />
                     )}
                 </>
             )}
